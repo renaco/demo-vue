37c48e5971e6b8a4f96138eac8d4f794efca7bf6
Renan Romero <renan.romero@orbis.com.pe>
2018-04-30 18:10:26 -0500

    wip app: learn directives

---
+++ src/components/DataBinding.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <input type="text">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'DataBinding',
+    props: {
+      message: String
+    }
+  }
+</script>
+
+<style>
+  h1 {
+    font-size: 2.5em;
+    color:cadetblue;
+  }
+</style>
---
+++ src/components/Directives.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="directives">
+    <h4>This is another component</h4>
+    <section v-if="conected"> This is my section </section>
+    <button v-on:click="showSection">Show section</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Directives',
+    methods: {
+      showSection: function() {
+        this.conected = true;
+      }
+    }
+  }
+</script>
